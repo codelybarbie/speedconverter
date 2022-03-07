f34aad79305dc3de182c04b10cf18bbf33be8518
sarangcoding <pinklybarbie@gmail.com>
2022-03-07 16:37:00 +0900

    SpeedConverter

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        long miles = SpeedConverter.toMilesPerHour(kilometersPerHour 10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(kilometersPerHour 10.5);
+    }
+}
